0ba766d1f6f398b4f0c8389fa279d592848617c7
rescoding <apriliadalapan03@gmail.com>
2023-02-28 17:54:34 +0800

    penambahan hello

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+/*root.render(
   <React.StrictMode>
-    <App />
+    <hello />
   </React.StrictMode>
-);
-
+);*/
+root.render(<h1>aprilia dalapan</h1>);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
